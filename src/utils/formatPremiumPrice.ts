9fdef6ebd25fb7681dc8617b290b68a3fb6c5ad9
encryptedDegen <encrypted@kodex.io>
2024-03-08 23:26:04 +0100

    Update formatPremiumPrice.ts

--- src/utils/formatPremiumPrice.ts
+++ src/utils/formatPremiumPrice.ts
@@ -2,7 +2,7 @@ export const formatRegisterPrice = (price: number) => {
   const million = Math.pow(10, 6)
 
   return price >= million
-    ? `$${Math.floor(price / million)}M`
+    ? `$ ${Math.floor(price / million)}M`
     : price.toLocaleString(navigator.language, {
         maximumFractionDigits: price >= 1000 ? 0 : price >= 100 ? 1 : 2,
       })
