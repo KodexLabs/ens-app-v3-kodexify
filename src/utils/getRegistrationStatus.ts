6d4a9ab369b388b486761b3acfab449c608e3ec4
encryptedDegen <encrypted@kodex.io>
2024-03-08 21:36:35 +0100

    change unregistered registration status

--- src/utils/getRegistrationStatus.ts
+++ src/utils/getRegistrationStatus.ts
@@ -13,5 +13,5 @@ export const getRegistrationStatus = (expireTime: number | null) => {
   if (timeSinceExpiry >= DAY_IN_SECONDS * 90 && timeSinceExpiry < DAY_IN_SECONDS * 111)
     return 'premium'
 
-  if (timeSinceExpiry > DAY_IN_SECONDS * 111) return 'unregistered'
+  if (timeSinceExpiry > DAY_IN_SECONDS * 111) return 'available'
 }
